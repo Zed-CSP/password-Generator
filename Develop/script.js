53465ff370cc5ec2c841b05c96d8e30f7bb71c28
Christopher Peret <cperet89@gmail.com>
2023-02-17 22:53:59 -0800

    password generator function created, with prompt criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//initial Password Generator Function
+function generatePassword() {
+
+// Prompt user for password length
+prompt("How many characters would you like your password to contain?");
+
+// Prompt user for character types
+prompt("Click OK to confirm including lowercase characters");
+prompt("Click OK to confirm including uppercase characters");
+prompt("Click OK to confirm including numeric characters");
+prompt("Click OK to confirm including special characters");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
