a2e014591b3b598b2418e6b89fc0d5409d26b938
Christopher Peret <cperet89@gmail.com>
2023-02-18 14:50:03 -0800

    added length validation, as while loop. Used isNaN for edge case

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,13 @@ function generatePassword() {
 // Prompt user for password length
 // parseint used to convert string to integer
 let passwordLength = parseInt(prompt("How many characters would you like your password to contain? (Must be 8-128) "));
+// if statement to check if password length is between 8 and 128
+// isNaN used to check (edge case) if input is a number
+while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+  passwordLength = parseInt(prompt("Invalid input. Please enter password length (8-128 characters):"));
+}
+
+
 
 // Prompt user for character types
 let includeLowercase = comfirm("Click OK to confirm including lowercase characters");
